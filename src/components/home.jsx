2a4927982f93fd29af49b5ff25bd420f741617b6
Ana Carolina <carol00_10@hotmail.com>
2025-02-14 14:33:40 -0300

    update

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -11,10 +11,12 @@ export default function Home() {
           <div className="text-white max-w-7xl text-right">
             <h2 className=" title-home text-[7rem] font-semibold mb-4">{t("home.title_h2")}</h2>
             <p className="text-2xl mb-6 title-home">{t("home.description")}</p>
-            <a
-              href="#contato"
-              className=" w-48 h-24 inline-block bg-[#0353a4] text-white text-2xl font-bold title-home rounded-md shadow-md px-[49px] py-7"
-            >{t("home.button")}</a>
+            <div className="flex justify-end items-center flex-nowrap flex-row">
+              <a
+                href="#contato"
+                className=" w-48 h-24 flex items-center justify-center flex-nowrap flex-row bg-[#0353a4] text-white text-2xl font-bold title-home rounded-md shadow-md"
+              >{t("home.button")}</a>
+            </div>
           </div>
         </div>
       </div>
